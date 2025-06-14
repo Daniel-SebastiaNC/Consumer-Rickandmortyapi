755edc938c4c53d42aeabcea91f8ef4061ef00f0
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-06-14 18:09:48 -0300

    feat: update home, use Card component and add content-conteiner style class

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { ApiService } from './../../api/api-service';
+import { Component, inject, OnInit } from '@angular/core';
+import { Card } from '../../shared/components/card/card';
+import { Person } from '../../shared/interfaces/person.interface';
 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Card],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements OnInit {
 
-}
+  persons!: Person[];
+
+  apiService = inject(ApiService);
+
+
+  ngOnInit(): void {
+    this.apiService.getAll().subscribe( (data) => {
+      this.persons = data.results;
+    });
+  }
+
+ }
